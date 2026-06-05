e8da677bbb0082960ff54f7762bcda0958d62c10
sofaspawn <h.m1nus.work@gmail.com>
2026-06-05 23:34:34 +0530

    statpadding

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:5173",
   "https://video-watchparty.vercel.app",
-  "https://video-watchparty-epnz6d4b3-sofaspawns-projects.vercel.app"
+  "https://video-watchparty-epnz6d4b3-sofaspawns-projects.vercel.app",
 ];
 
 const io = new Server(server, {
